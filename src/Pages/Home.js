d0f5986abc29f02a299c4756668ac6c0445863e8
dharakachhadia <dharakachhadia@gmail.com>
2022-12-22 18:26:50 +0530

    worked on other section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,11 +1,20 @@
 import React from "react";
-import styled from "styled-components";
+import HeroSection from "../components/HeroSection";
+import Services from "../components/Services";
+import Trusted from "../components/Trusted";
+
 const Home = () => {
-  return <Wrapper className="test">Home</Wrapper>;
+  const data = {
+    name: "thapa store",
+  };
+
+  return (
+    <>
+      <HeroSection myData={data} />
+      <Services />
+      <Trusted />
+    </>
+  );
 };
-
-const Wrapper = styled.section`
-
-`;
 
 export default Home;
