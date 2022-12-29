178897c4bfb6f9a2a6d90f41d4d55b56530509ea
dharakachhadia <dharakachhadia@gmail.com>
2022-12-29 22:17:57 +0530

    confict

--- src/context/productcontex.js
+++ src/context/productcontex.js
@@ -1,49 +1,51 @@
-import axios from 'axios';
-import { createContext, useContext, useEffect, useReducer } from 'react';
-import reducer from '../reducer/productReducer';
+import { createContext, useContext, useEffect, useReducer } from "react";
+import axios from "axios";
+import reducer from "../reducer/productReducer";
+
 const AppContext = createContext();
-const API = 'https://api.pujakaitem.com/api/products';
+
+const API = "https://api.pujakaitem.com/api/products";
 
 const initialState = {
   isLoading: false,
   isError: false,
   products: [],
   featureProducts: [],
+  isSingleLoading: false,
   singleProduct: {},
 };
 
 const AppProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialState);
 
-  const getProducts = async url => {
-    dispatch({ type: 'SET_LOADING' });
+  const getProducts = async (url) => {
+    dispatch({ type: "SET_LOADING" });
     try {
       const res = await axios.get(url);
       const products = await res.data;
-      dispatch({ type: 'SET_API_DATA', payload: products });
+      dispatch({ type: "SET_API_DATA", payload: products });
     } catch (error) {
-      dispatch({ type: 'API_ERROR' });
+      dispatch({ type: "API_ERROR" });
     }
   };
 
-  // my second api data call for single data
+  // my 2nd api call for single product
 
-  const getSingleProduct = async url => {
-    dispatch({ type: 'SET_LOADING' });
-
+  const getSingleProduct = async (url) => {
+    dispatch({ type: "SET_SINGLE_LOADING" });
     try {
       const res = await axios.get(url);
       const singleProduct = await res.data;
-      dispatch({ type: 'SET_SINGLE_PRODUCT', payload: singleProduct });
-      console.log('===============================', singleProduct);
+      dispatch({ type: "SET_SINGLE_PRODUCT", payload: singleProduct });
     } catch (error) {
-      dispatch({ type: 'API_ERROR' });
+      dispatch({ type: "SET_SINGLE_ERROR" });
     }
   };
 
   useEffect(() => {
     getProducts(API);
   }, []);
+
   return (
     <AppContext.Provider value={{ ...state, getSingleProduct }}>
       {children}
@@ -51,9 +53,9 @@ const AppProvider = ({ children }) => {
   );
 };
 
-//custom hooks for
-
+// custom hooks
 const useProductContext = () => {
   return useContext(AppContext);
 };
+
 export { AppProvider, AppContext, useProductContext };
