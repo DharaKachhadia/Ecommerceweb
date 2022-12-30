f939ec1479e9fe637bb34ab023f27c5a32f4601f
dharakachhadia <dharakachhadia@gmail.com>
2022-12-30 18:06:10 +0530

    worked on product section

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { ThemeProvider } from "styled-components";
-import "./App.css";
+import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import {ThemeProvider} from 'styled-components';
+
+import './App.css';
 import {
   Home,
   About,
@@ -10,26 +11,31 @@ import {
   SingleProduct,
   Cart,
   ErrorPage,
-} from "./Pages/index";
-import { GlobalStyle } from "./GlobalStyle";
-import Header from "./components/Header";
+} from './Pages/index';
+import {GlobalStyle} from './GlobalStyle';
+import Header from './components/Header';
+import Footer from './components/Footer';
 const App = () => {
   const theme = {
     colors: {
-      bg: "#F6F8FA",
-      footer_bg: "#0a1435",
-      btn: "rgb(98 84 243)",
-      border: "rgba(98, 84, 243, 0.5)",
-      hr: "#ffffff",
-      gradient:
-        "Linear-gradient(Odeg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)",
-      shadow:
-        "rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;",
-      shadowSupport: "rgba(0, 0, 0, 0.16) 0px 1px 4px",
+      heading: 'rgb(24 24 29)',
+      text: 'rgba(29 ,29, 29, .8)',
+      white: '#fff',
+      black: ' #212529',
+      helper: '#8490ff',
+
+      bg: '#F6F8FA',
+      footer_bg: '#0a1435',
+      btn: 'rgb(98 84 243)',
+      border: 'rgba(98, 84, 243, 0.5)',
+      hr: '#ffffff',
+      gradient: 'linear-gradient(0deg, rgb(132 144 255) 0%, rgb(98 189 252) 100%)',
+      shadow: 'rgba(0, 0, 0, 0.02) 0px 1px 3px 0px,rgba(27, 31, 35, 0.15) 0px 0px 0px 1px;',
+      shadowSupport: ' rgba(0, 0, 0, 0.16) 0px 1px 4px',
     },
     media: {
-      mobile: "768px",
-      tab: "998px",
+      mobile: '768px',
+      tab: '998px',
     },
   };
 
@@ -47,6 +53,7 @@ const App = () => {
           <Route path="/cart" element={<Cart />} />
           <Route path="*" element={<ErrorPage />} />
         </Routes>
+        <Footer />
       </Router>
     </ThemeProvider>
   );
